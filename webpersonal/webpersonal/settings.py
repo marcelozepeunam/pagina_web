a7a0a46d2dd74039addcb1c779dec46582b08deb
Marcelo Zepeda <marcelozepe.unam@gmail.com>
2024-07-08 20:29:00 -0600

    Modificacion settings.py

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -112,6 +112,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Media files
 MEDIA_URL = '/media/'
